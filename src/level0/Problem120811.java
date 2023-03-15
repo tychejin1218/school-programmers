bc351685c8fe2e3491db0421a296a63757b0c868
너나들이 <tychejin@naver.com>
2023-03-15 13:25:32 +0900

    Chore: 코딩테스트 연습
    문제_120811 : 중앙값 구하기

---
+++ src/level0/Problem120811.java
@@ -0,0 +1,16 @@
+package level0;
+
+import java.util.Arrays;
+
+/**
+ * 중앙값 구하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120811
+ */
+public class Problem120811 {
+
+  public int solution(int[] array) {
+    Arrays.sort(array);
+    return array[array.length / 2];
+  }
+}
