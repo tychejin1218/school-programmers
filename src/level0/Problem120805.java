ba62b3ef6bdbaed3e1c2b518151c36df76ad4258
너나들이 <tychejin@naver.com>
2023-03-16 13:18:31 +0900

    Chore: 코딩테스트 연습
    문제_120805 : 몫 구하기

---
+++ src/level0/Problem120805.java
@@ -0,0 +1,13 @@
+package level0;
+
+/**
+ * 몫 구하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120804
+ */
+public class Problem120805 {
+
+  public int solution(int num1, int num2) {
+    return num1 / num2;
+  }
+}
