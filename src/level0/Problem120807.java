e8c8c8ab8d320455383c235cb18ecadee3132160
너나들이 <tychejin@naver.com>
2023-03-17 12:37:04 +0900

    Chore: 코딩테스트 연습
    문제_120807 : 숫자 비교하기

---
+++ src/level0/Problem120807.java
@@ -0,0 +1,13 @@
+package level0;
+
+/**
+ * 두 수의 나눗셈
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120806
+ */
+public class Problem120807 {
+
+  public int solution(int num1, int num2) {
+    return num1 == num2 ? 1 : -1;
+  }
+}
---
+++ test/level0/Problem120807Test.java
@@ -0,0 +1,53 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120807Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int num1 = 2;
+    int num2 = 3;
+    int expected = -1;
+
+    // When
+    int result = new Problem120807().solution(num1, num2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int num1 = 11;
+    int num2 = 11;
+    int expected = 1;
+
+    // When
+    int result = new Problem120807().solution(num1, num2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    int num1 = 7;
+    int num2 = 99;
+    int expected = -1;
+
+    // When
+    int result = new Problem120807().solution(num1, num2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
