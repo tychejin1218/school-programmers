1144b1834912f8d3589b380626fb4304ddaf2b82
너나들이 <tychejin@naver.com>
2023-03-18 01:04:38 +0900

    Chore: 코딩테스트 연습
    문제_120814 : 피자 나눠 먹기 (1)

---
+++ test/level0/Problem120814Test.java
@@ -0,0 +1,50 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120814Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int n = 7;
+    int expected = 1;
+
+    // When
+    int result = new Problem120814().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int n = 1;
+    int expected = 1;
+
+    // When
+    int result = new Problem120814().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    int n = 15;
+    int expected = 3;
+
+    // When
+    int result = new Problem120814().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
