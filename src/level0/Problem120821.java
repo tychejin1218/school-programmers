8012f085a0da43fa9a766749d0fb0acb9e2f4960
너나들이 <tychejin@naver.com>
2023-03-22 08:37:18 +0900

    Chore: 코딩테스트 연습
    문제_120821 : 배열 뒤집기

---
+++ src/level0/Problem120821.java
@@ -0,0 +1,24 @@
+package level0;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 배열 뒤집기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120821
+ */
+public class Problem120821 {
+
+  public int[] solution(int[] num_list) {
+    List<Integer> list = Arrays.stream(num_list)
+        .boxed()
+        .collect(Collectors.toList());
+    Collections.reverse(list);
+    return list.stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+  }
+}
