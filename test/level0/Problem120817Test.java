12401f27f2d0e3d0233c8e635f78f9c58d0f08a8
너나들이 <tychejin@naver.com>
2023-03-25 22:49:38 +0900

    Chore: 코딩테스트 연습
    문제_120817 : 배열의 평균값

---
+++ test/level0/Problem120817Test.java
@@ -0,0 +1,22 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120817Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double expected = 5.5;
+
+    // When
+    double result = new Problem120817().solution(numbers);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
