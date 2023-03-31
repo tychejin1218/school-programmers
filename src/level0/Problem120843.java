343557420604e91319d39ec0a6ffa3255e601b00
너나들이 <tychejin@naver.com>
2023-04-01 01:23:04 +0900

    Chore: 코딩테스트 연습
    문제_120843 : 공 던지기

--- src/level0/Problem120843.java
+++ src/level0/Problem120843.java
@@ -7,8 +7,30 @@ package level0;
  */
 public class Problem120843 {
 
-  public int solution(int[] numbers, int k) {
+  /*public int solution(int[] numbers, int k) {
     int answer = 0;
+
+    int order = 0;
+    int repetition = 1;
+    for (int a = 0; a < k; a++) {
+
+      if (repetition == k) {
+        answer = numbers[order];
+        break;
+      } else {
+        repetition++;
+      }
+
+      order = order + 2;
+      if (order >= numbers.length) {
+        order = order - numbers.length;
+      }
+    }
+
     return answer;
+  }*/
+
+  public int solution(int[] numbers, int k) {
+    return numbers[((1 + ((k - 1) * 2)) % numbers.length) - 1];
   }
 }
