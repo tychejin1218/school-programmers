94b49d2a32fd9cb669e027f53851d38673428a6f
너나들이 <tychejin@naver.com>
2023-03-31 17:32:44 +0900

    Chore: 코딩테스트 연습
    문제_120842 : 2차원으로 만들기

---
+++ test/level0/Problem120842Test.java
@@ -0,0 +1,38 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120842Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] num_list = {1, 2, 3, 4, 5, 6, 7, 8};
+    int n = 2;
+    int[][] expected = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+
+    // When
+    int[][] result = new Problem120842().solution(num_list, n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] num_list = {100, 95, 2, 4, 5, 6, 18, 33, 948};
+    int n = 3;
+    int[][] expected = {{100, 95, 2}, {4, 5, 6}, {18, 33, 948}};
+
+    // When
+    int[][] result = new Problem120842().solution(num_list, n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
