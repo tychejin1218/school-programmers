55498665b9b9f1339cc0c453095ac8c74312f4ab
너나들이 <tychejin@naver.com>
2023-03-31 12:45:16 +0900

    Chore: 코딩테스트 연습
    문제_120841 : 점의 위치 구하기

---
+++ src/level0/Problem120841.java
@@ -0,0 +1,32 @@
+package level0;
+
+/**
+ * 점의 위치 구하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120841
+ */
+public class Problem120841 {
+
+  public int solution(int[] dot) {
+    int answer = 0;
+
+    int x = dot[0];
+    int y = dot[1];
+
+    if (x > 0) {
+      if (y > 0) {
+        answer = 1;
+      } else {
+        answer = 4;
+      }
+    } else {
+      if (y > 0) {
+        answer = 2;
+      } else {
+        answer = 3;
+      }
+    }
+
+    return answer;
+  }
+}
