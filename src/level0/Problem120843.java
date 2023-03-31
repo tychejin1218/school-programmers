53999105ba08a273730caa53c836baf3345675a8
너나들이 <tychejin@naver.com>
2023-03-31 17:56:41 +0900

    Chore: 코딩테스트 연습
    문제_120843 : 공 던지기

---
+++ src/level0/Problem120843.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 공 던지기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120843
+ */
+public class Problem120843 {
+
+  public int solution(int[] numbers, int k) {
+    int answer = 0;
+    return answer;
+  }
+}
