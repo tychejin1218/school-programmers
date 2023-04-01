e7e0e655afab757be5cbaa6af153edc5d71a0228
너나들이 <tychejin@naver.com>
2023-04-02 00:04:43 +0900

    Chore: 코딩테스트 연습
    문제_120844 : 배열 회전시키기

---
+++ src/level0/Problem120844.java
@@ -0,0 +1,31 @@
+package level0;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 배열 회전시키기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120843
+ */
+public class Problem120844 {
+
+  public int[] solution(int[] numbers, String direction) {
+
+    List<Integer> numberList = Arrays.stream(numbers)
+        .boxed()
+        .collect(Collectors.toList());
+
+    if ("right".equals(direction)) {
+      Collections.rotate(numberList, 1);
+    } else {
+      Collections.rotate(numberList, -1);
+    }
+
+    return numberList.stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+  }
+}
