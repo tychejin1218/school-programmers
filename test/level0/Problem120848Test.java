b373f2d8e481999bd4f6b592e52fe0ce08f96488
너나들이 <tychejin@naver.com>
2023-04-04 13:45:22 +0900

    Chore: 코딩테스트 연습
    문제_120848 : 팩토리얼

---
+++ test/level0/Problem120848Test.java
@@ -0,0 +1,36 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120848Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int n = 3628800;
+    int expected = 10;
+
+    // When
+    int result = new Problem120848().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int n = 7;
+    int expected = 3;
+
+    // When
+    int result = new Problem120848().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
