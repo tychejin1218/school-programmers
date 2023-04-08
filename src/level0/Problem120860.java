5395da9ac371b54cc56b63d4e1485f06e7f14c00
너나들이 <tychejin@naver.com>
2023-04-08 22:33:58 +0900

    Chore: 코딩테스트 연습
    문제_120860 : 직사각형 넓이 구하기

---
+++ src/level0/Problem120860.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 직사각형 넓이 구하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120860
+ */
+public class Problem120860 {
+
+  public int solution(int[][] dots) {
+    int answer = 0;
+    return answer;
+  }
+}
