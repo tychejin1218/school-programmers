91ed3fbfdbbf072283cf5881a632025d32a2f242
너나들이 <tychejin@naver.com>
2023-04-08 22:30:44 +0900

    Chore: 코딩테스트 연습
    문제_120854 : 배열 원소의 길이

---
+++ src/level0/Problem120854.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 배열 원소의 길이
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120854
+ */
+public class Problem120854 {
+
+  public int[] solution(String[] strlist) {
+    int[] answer = {};
+    return answer;
+  }
+}
---
+++ test/level0/Problem120854Test.java
@@ -0,0 +1,36 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120854Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String[] strlist = {"We", "are", "the", "world!"};
+    int[] expected = {2, 3, 3, 6};
+
+    // When
+    int[] result = new Problem120854().solution(strlist);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String[] strlist = {"I", "Love", "Programmers."};
+    int[] expected = {1, 4, 12};
+
+    // When
+    int[] result = new Problem120854().solution(strlist);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
