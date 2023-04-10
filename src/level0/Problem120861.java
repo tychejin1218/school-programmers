49dc51dfd43b23e97fed929d84075bd4bd026168
너나들이 <tychejin@naver.com>
2023-04-10 10:37:37 +0900

    Chore: 코딩테스트 연습
    문제_120861 : 캐릭터의 좌표

--- src/level0/Problem120861.java
+++ src/level0/Problem120861.java
@@ -8,7 +8,38 @@ package level0;
 public class Problem120861 {
 
   public int[] solution(String[] keyinput, int[] board) {
-    int[] answer = {};
+
+    String keyValue = "";
+    int positionX = 0;
+    int positionY = 0;
+    int maxPositionX = board[0] / 2;
+    int minPositionX = maxPositionX * -1;
+    int maxPositionY = board[1] / 2;
+    int minPositionY = maxPositionY * -1;
+
+    for (int a = 0; a < keyinput.length; a++) {
+
+      keyValue = keyinput[a];
+
+      if (positionX < maxPositionX && positionX > minPositionX) {
+        if ("right".equals(keyValue)) {
+          positionX = positionX + 1;
+        } else if ("left".equals(keyValue)) {
+          positionX = positionX - 1;
+        }
+      }
+
+      if (positionY < maxPositionY && positionY > minPositionY) {
+        if ("up".equals(keyValue)) {
+          positionY = positionY + 1;
+        } else if ("down".equals(keyValue)) {
+          positionY = positionY - 1;
+        }
+      }
+    }
+
+    int[] answer = {positionX, positionY};
+
     return answer;
   }
 }
