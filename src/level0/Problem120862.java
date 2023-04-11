28739b9f1dd4def5a9a35c6c97ed8d285c0b45a2
너나들이 <tychejin@naver.com>
2023-04-11 11:59:52 +0900

    Chore: 코딩테스트 연습
    문제_120862 : 최댓값 만들기 (2)

---
+++ src/level0/Problem120862.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 최댓값 만들기 (2)
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120862
+ */
+public class Problem120862 {
+
+  public int solution(int[] numbers) {
+    int answer = 0;
+    return answer;
+  }
+}
