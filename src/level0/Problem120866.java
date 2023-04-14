7ac387afb81e73b72231e313ba754d88f6d1788a
너나들이 <tychejin@naver.com>
2023-04-14 09:02:21 +0900

    Chore: 코딩테스트 연습
    문제_120866 : 안전지대

---
+++ src/level0/Problem120866.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 안전지대
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120866
+ */
+public class Problem120866 {
+
+  public int solution(int[][] board) {
+    int answer = 0;
+    return answer;
+  }
+}
