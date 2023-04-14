d565f1c6ded3cce57f8e5ceac11448b6831d7047
너나들이 <tychejin@naver.com>
2023-04-14 13:05:39 +0900

    Chore: 코딩테스트 연습
    문제_120868 : 삼각형의 완성조건 (2)

---
+++ src/level0/Problem120868.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 삼각형의 완성조건 (2)
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120868
+ */
+public class Problem120868 {
+
+  public int solution(int[] sides) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem120868Test.java
@@ -0,0 +1,51 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120868Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] sides = {1, 2};
+    int expected = 1;
+
+    // When
+    int result = new Problem120868().solution(sides);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] sides = {3, 6};
+    int expected = 15
+
+    // When
+    int result = new Problem120868().solution(sides);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    int[] sides = {11, 7};
+    int expected = 13;
+
+    // When
+    int result = new Problem120868().solution(sides);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+}
