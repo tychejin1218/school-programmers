69b45ac50c113ab5bd2ba987b3d38346c1876738
너나들이 <tychejin@naver.com>
2023-04-15 23:10:33 +0900

    Chore: 코딩테스트 연습
    문제_120869 : 외계어 사전

---
+++ src/level0/Problem120869.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 외계어 사전
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120869
+ */
+public class Problem120869 {
+
+  public int solution(String[] spell, String[] dic) {
+    int answer = 0;
+    return answer;
+  }
+}
