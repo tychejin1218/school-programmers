97fbf25d240e42556e32551a41f63cde45494b3f
너나들이 <tychejin@naver.com>
2023-04-15 23:41:01 +0900

    Chore: 코딩테스트 연습
    문제_120869 : 외계어 사전

--- src/level0/Problem120869.java
+++ src/level0/Problem120869.java
@@ -1,5 +1,9 @@
 package level0;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 외계어 사전
  * <p/>
@@ -7,7 +11,7 @@ package level0;
  */
 public class Problem120869 {
 
-  public int solution(String[] spell, String[] dic) {
+  public int solution01(String[] spell, String[] dic) {
 
     int answer = 2;
 
@@ -27,5 +31,23 @@ public class Problem120869 {
     }
 
     return answer;
+  }
+
+  public int solution(String[] spell, String[] dic) {
+
+    List<String> dicList = Arrays.stream(dic)
+        .map(m -> m.chars()
+            .sorted()
+            .mapToObj(i -> String.valueOf((char) i))
+            .collect(Collectors.joining()))
+        .collect(Collectors.toList());
+    System.out.println(dicList);
+
+    String spellStr = Arrays.stream(spell)
+        .sorted()
+        .collect(Collectors.joining());
+    System.out.println(spellStr);
+
+    return dicList.contains(spellStr) ? 1 : 2;
   }
 }
