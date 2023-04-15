18c4c7e6a164e62e408323727d765b7ce202b1c7
너나들이 <tychejin@naver.com>
2023-04-15 23:35:02 +0900

    Chore: 코딩테스트 연습
    문제_120869 : 외계어 사전

--- src/level0/Problem120869.java
+++ src/level0/Problem120869.java
@@ -10,10 +10,10 @@ public class Problem120869 {
   public int solution(String[] spell, String[] dic) {
 
     int answer = 2;
-    int check = 0;
 
     for (String dicStr : dic) {
 
+      int check = 0;
       for (String spellStr : spell) {
         if (dicStr.toString().indexOf(spellStr) != -1) {
           check++;
@@ -23,8 +23,6 @@ public class Problem120869 {
       if (check == spell.length) {
         answer = 1;
         break;
-      } else {
-        check = 0;
       }
     }
 
