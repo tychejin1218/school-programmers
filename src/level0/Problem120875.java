5ef18781d76da0e5000d7636dd5f5ed095caa4e9
너나들이 <tychejin@naver.com>
2023-04-18 12:30:06 +0900

    Chore: 코딩테스트 연습
    문제_120875 : 평행

--- src/level0/Problem120875.java
+++ src/level0/Problem120875.java
@@ -20,26 +20,24 @@ public class Problem120875 {
     int x4 = dots[3][0];
     int y4 = dots[3][1];
 
-    int distanceX21 = Math.abs(x2 - x1);
-    int distanceX43 = Math.abs(x4 - x3);
-    int distanceY21 = Math.abs(y2 - y1);
-    int distanceY43 = Math.abs(y4 - y3);
+    int distanceX21 = x2 - x1;
+    int distanceX43 = x4 - x3;
+    int distanceY21 = y2 - y1;
+    int distanceY43 = y4 - y3;
+    int distanceX31 = x3 - x1;
+    int distanceX42 = x4 - x2;
+    int distanceY31 = y3 - y1;
+    int distanceY42 = y4 - y2;
+    int distanceX41 = x4 - x1;
+    int distanceX32 = x3 - x2;
+    int distanceY41 = y4 - y1;
+    int distancey32 = y3 - y2;
 
-    int distanceX31 = Math.abs(x3 - x1);
-    int distanceX42 = Math.abs(x4 - x2);
-    int distanceY31 = Math.abs(y3 - y1);
-    int distanceY42 = Math.abs(y4 - y2);
-
-    int distanceX41 = Math.abs(x4 - x1);
-    int distanceX32 = Math.abs(x3 - x2);
-    int distanceY41 = Math.abs(y4 - y1);
-    int distancey32 = Math.abs(y3 - y2);
-
-    if (distanceX21 == distanceX21 && distanceY21 == distanceY43) {
+    if ((double) distanceY21 / distanceX21 == (double) distanceY43 / distanceX43) {
       answer = 1;
-    } else if (distanceX31 == distanceX42 && distanceY31 == distanceY42) {
+    } else if ((double) distanceY31 / distanceX31 == (double) distanceY42 / distanceX42) {
       answer = 1;
-    } else if (distanceX41 == distanceX32 && distanceY41 == distancey32) {
+    } else if ((double) distanceY41 / distanceX41 == (double) distancey32 / distancey32) {
       answer = 1;
     }
 
