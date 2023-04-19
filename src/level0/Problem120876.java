767009429cb1bb6f2a8e17f4e5640f0d67512597
너나들이 <tychejin@naver.com>
2023-04-19 09:57:18 +0900

    Chore: 코딩테스트 연습
    문제_120876 : 겹치는 선분의 길이

---
+++ src/level0/Problem120876.java
@@ -0,0 +1,30 @@
+package level0;
+
+/**
+ * 겹치는 선분의 길이
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120876
+ */
+public class Problem120876 {
+
+  public int solution(int[][] lines) {
+
+    int answer = 0;
+
+    int[] arr = new int[200];
+
+    for (int a = 0; a < lines.length; a++) {
+      for (int b = lines[a][0] + 100; b < lines[a][1] + 100; b++) {
+        arr[b]++;
+      }
+    }
+
+    for (int a = 0; a < 200; a++) {
+      if (arr[a] > 1) {
+        answer++;
+      }
+    }
+
+    return answer;
+  }
+}
