f4a0314b13f86275f3151dcdb81bb7e7f5830aae
너나들이 <tychejin@naver.com>
2023-04-22 23:24:44 +0900

    Chore: 코딩테스트 연습
    문제_120883 : 로그인 성공?

---
+++ src/level0/Problem120883.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 로그인 성공?
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120882
+ */
+public class Problem120883 {
+
+  public String solution(String[] id_pw, String[][] db) {
+    String answer = "";
+    return answer;
+  }
+}
