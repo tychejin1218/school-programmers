367464ca47257d001967ab3fe6d65ef15e3b01d7
너나들이 <tychejin@naver.com>
2023-04-22 23:41:14 +0900

    Chore: 코딩테스트 연습
    문제_120883 : 로그인 성공?

--- src/level0/Problem120883.java
+++ src/level0/Problem120883.java
@@ -9,6 +9,18 @@ public class Problem120883 {
 
   public String solution(String[] id_pw, String[][] db) {
     String answer = "";
-    return answer;
+
+    for (int a = 0; a < db.length; a++) {
+      if (id_pw[0].equals(db[a][0])) {
+        if (id_pw[1].equals(db[a][1])) {
+          answer = "login";
+        } else {
+          answer = "wrong pw";
+        }
+        break;
+      }
+    }
+
+    return "".equals(answer) ? "fail" : answer;
   }
 }
