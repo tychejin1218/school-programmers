0fff6d8f83d64ed6272fe8eaf5cd79c3ebf1070a
너나들이 <tychejin@naver.com>
2023-04-23 17:57:03 +0900

    Chore: 코딩테스트 연습
    문제_120884 : 치킨 쿠폰

---
+++ src/level0/Problem120884.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 치킨 쿠폰
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120884
+ */
+public class Problem120884 {
+
+  public int solution(int chicken) {
+    int answer = -1;
+    return answer;
+  }
+}
