8ec750b859fe72e4f86b82dc9adf50479d5c77b2
너나들이 <tychejin@naver.com>
2023-04-23 18:26:30 +0900

    Chore: 코딩테스트 연습
    문제_120884 : 치킨 쿠폰

--- src/level0/Problem120884.java
+++ src/level0/Problem120884.java
@@ -8,7 +8,24 @@ package level0;
 public class Problem120884 {
 
   public int solution(int chicken) {
-    int answer = -1;
+    int answer = 0;
+
+    int serviceChicken = chicken / 10;
+    int coupon = serviceChicken + (chicken % 10);
+
+    while (true) {
+
+      answer += serviceChicken;
+
+      if (coupon < 10) {
+        break;
+      }
+
+      serviceChicken = coupon / 10;
+      coupon = serviceChicken + (coupon % 10);
+
+    }
+
     return answer;
   }
 }
