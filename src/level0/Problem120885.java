4f0db66a08d11f560289a1580315b0b4cf72cfd3
너나들이 <tychejin@naver.com>
2023-04-24 07:42:12 +0900

    Chore: 코딩테스트 연습
    문제_120885 : 이진수 더하기

---
+++ src/level0/Problem120885.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 이진수 더하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120885
+ */
+public class Problem120885 {
+
+  public String solution(String bin1, String bin2) {
+    String answer = "";
+    return answer;
+  }
+}
---
+++ test/level0/Problem120885Test.java
@@ -0,0 +1,38 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120885Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String bin1 = "10";
+    String bin2 = "11";
+    String expected = "result";
+
+    // When
+    String result = new Problem120885().solution(bin1, bin2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String bin1 = "1001";
+    String bin2 = "1111";
+    String expected = "11000";
+
+    // When
+    String result = new Problem120885().solution(bin1, bin2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
