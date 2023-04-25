1dda1790031009a2f40e470d8d7b22cf90ade88d
너나들이 <tychejin@naver.com>
2023-04-25 12:26:15 +0900

    Chore: 코딩테스트 연습
    문제_120887 : k의 개수

---
+++ src/level0/Problem120887.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * k의 개수
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120887
+ */
+public class Problem120887 {
+
+  public int solution(int i, int j, int k) {
+    int answer = 0;
+    return answer;
+  }
+}
