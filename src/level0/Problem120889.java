23aa366ebb0345159f08797ddf6022cd55ffe5a6
너나들이 <tychejin@naver.com>
2023-04-26 10:04:50 +0900

    Chore: 코딩테스트 연습
    문제_120889 : 삼각형의 완성조건 (1)

---
+++ src/level0/Problem120889.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 삼각형의 완성조건 (1)
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120889
+ */
+public class Problem120889 {
+
+  public int solution(int[] sides) {
+    int answer = 0;
+    return answer;
+  }
+}
