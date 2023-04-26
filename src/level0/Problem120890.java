8a1b9dfc0bb22c0db4d55e90a5f0fe6c6c59a46d
너나들이 <tychejin@naver.com>
2023-04-26 13:30:36 +0900

    Chore: 코딩테스트 연습
    문제_120890 : 가까운 수

--- src/level0/Problem120890.java
+++ src/level0/Problem120890.java
@@ -1,5 +1,10 @@
 package level0;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 가까운 수
  * <p/>
@@ -9,6 +14,39 @@ public class Problem120890 {
 
   public int solution(int[] array, int n) {
     int answer = 0;
+
+    List<Integer> nums = Arrays.stream(array)
+        .boxed()
+        .collect(Collectors.toList());
+
+    if (nums.indexOf(n) != -1) {
+      answer = n;
+    } else {
+
+      nums.add(n);
+
+      Collections.sort(nums);
+
+      int index = nums.indexOf(n);
+      if (index == 0) {
+        answer = nums.get(index + 1);
+      } else if (index == nums.size() - 1) {
+        answer = nums.get(index - 1);
+      } else {
+        int num1 = nums.get(index - 1);
+        int num2 = nums.get(index + 1);
+        int distance1 = Math.abs(n - num1);
+        int distance2 = Math.abs(n - num2);
+        if (distance1 > distance2) {
+          answer = num2;
+        } else if (distance1 < distance2) {
+          answer = num1;
+        } else {
+          answer = num1;
+        }
+      }
+    }
+
     return answer;
   }
 }
--- test/level0/Problem120890Test.java
+++ test/level0/Problem120890Test.java
@@ -35,4 +35,49 @@ class Problem120890Test {
     // Then
     assertEquals(expected, result);
   }
+
+  @Test
+  void solution03() {
+
+    // Given
+    int[] array = {10, 11, 12};
+    int n = 9;
+    int expected = 10;
+
+    // When
+    int result = new Problem120890().solution(array, n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution04() {
+
+    // Given
+    int[] array = {3, 10, 28};
+    int n = 18;
+    int expected = 10;
+
+    // When
+    int result = new Problem120890().solution(array, n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution05() {
+
+    // Given
+    int[] array = {3, 10, 28};
+    int n = 10;
+    int expected = 10;
+
+    // When
+    int result = new Problem120890().solution(array, n);
+
+    // Then
+    assertEquals(expected, result);
+  }
 }
