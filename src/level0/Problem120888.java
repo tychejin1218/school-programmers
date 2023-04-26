1988cb15ef7f1e24869e1671daf522de1a5fe58b
너나들이 <tychejin@naver.com>
2023-04-26 09:18:29 +0900

    Chore: 코딩테스트 연습
    문제_120888 : 중복된 문자 제거

---
+++ src/level0/Problem120888.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 중복된 문자 제거
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120888
+ */
+public class Problem120888 {
+
+  public String solution(String my_string) {
+    String answer = "";
+    return answer;
+  }
+}
