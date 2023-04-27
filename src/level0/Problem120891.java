bf42451998506e40adc6a8c3bdd25513a22f57e8
너나들이 <tychejin@naver.com>
2023-04-27 09:11:39 +0900

    Chore: 코딩테스트 연습
    문제_120891 : 369게임

---
+++ src/level0/Problem120891.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 369게임
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120891
+ */
+public class Problem120891 {
+
+  public int solution(int order) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem120891Test.java
@@ -0,0 +1,36 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120891Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int order = 3;
+    int expected = 1;
+
+    // When
+    int result = new Problem120891().solution(order);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int order = 29423;
+    int expected = 2;
+
+    // When
+    int result = new Problem120891().solution(order);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
