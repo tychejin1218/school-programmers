4f7bc3a642a26e7ddda10a2d6b91b68f9f60a6d1
너나들이 <tychejin@naver.com>
2023-04-27 17:58:26 +0900

    Chore: 코딩테스트 연습
    문제_120890 : 가까운 수

--- src/level0/Problem120890.java
+++ src/level0/Problem120890.java
@@ -49,4 +49,27 @@ public class Problem120890 {
 
     return answer;
   }
+
+  public int solution01(int[] array, int n) {
+    return Math.min(
+        array[
+            Arrays.stream(array)
+                .map(operand -> Math.abs(n - operand))
+                .boxed()
+                .collect(Collectors.toList())
+                .indexOf(
+                    Arrays.stream(array)
+                        .map(operand -> Math.abs(n - operand))
+                        .min().orElse(0))],
+        array[
+            Arrays.stream(array)
+                .map(operand -> Math.abs(n - operand))
+                .boxed()
+                .collect(Collectors.toList())
+                .lastIndexOf(
+                    Arrays.stream(array)
+                        .map(operand -> Math.abs(n - operand))
+                        .min().orElse(0))]
+    );
+  }
 }
