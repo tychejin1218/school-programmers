36c066fac8213b4b23ebfc941acbff4fe5b145d4
너나들이 <tychejin@naver.com>
2023-04-30 20:24:22 +0900

    Chore: 코딩테스트 연습
    문제_120895 : 인덱스 바꾸기

---
+++ src/level0/Problem120895.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 인덱스 바꾸기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120895
+ */
+public class Problem120895 {
+
+  public String solution(String my_string, int num1, int num2) {
+    String answer = "";
+    return answer;
+  }
+}
