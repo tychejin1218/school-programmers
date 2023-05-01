ba7d85c6b07a657f220d64cd628bb3b11634adc9
너나들이 <tychejin@naver.com>
2023-05-01 18:12:29 +0900

    Chore: 코딩테스트 연습
    문제_120896 : 한 번만 등장한 문자

---
+++ src/level0/Problem120896.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 한 번만 등장한 문자
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120896
+ */
+public class Problem120896 {
+
+  public String solution(String s) {
+    String answer = "";
+    return answer;
+  }
+}
---
+++ test/level0/Problem120896Test.java
@@ -0,0 +1,50 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120896Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String s = "abcabcadc";
+    String expected = "d";
+
+    // When
+    String result = new Problem120896().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String s = "abdc";
+    String expected = "abcd";
+
+    // When
+    String result = new Problem120896().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    String s = "hello";
+    String expected = "eho";
+
+    // When
+    String result = new Problem120896().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
