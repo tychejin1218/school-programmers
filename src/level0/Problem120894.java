7c9773126267fd9326415d8b5c00f8c0a8b400eb
너나들이 <tychejin@naver.com>
2023-05-02 09:11:23 +0900

    Chore: 코딩테스트 연습
    문제_120893 : 영어가 싫어요

--- src/level0/Problem120894.java
+++ src/level0/Problem120894.java
@@ -7,8 +7,32 @@ package level0;
  */
 public class Problem120894 {
 
+  public long solution01(String numbers) {
+
+    numbers = numbers.replaceAll("zero", String.valueOf(0));
+    numbers = numbers.replaceAll("one", String.valueOf(1));
+    numbers = numbers.replaceAll("two", String.valueOf(2));
+    numbers = numbers.replaceAll("three", String.valueOf(3));
+    numbers = numbers.replaceAll("four", String.valueOf(4));
+    numbers = numbers.replaceAll("five", String.valueOf(5));
+    numbers = numbers.replaceAll("six", String.valueOf(6));
+    numbers = numbers.replaceAll("seven", String.valueOf(7));
+    numbers = numbers.replaceAll("eight", String.valueOf(8));
+    numbers = numbers.replaceAll("nine", String.valueOf(9));
+
+    return Long.parseLong(numbers);
+  }
+
   public long solution(String numbers) {
-    long answer = 0;
-    return answer;
+
+    String[] nubmerStrs = {
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+
+    for (int a = 0; a < nubmerStrs.length; a++) {
+      numbers = numbers.replaceAll(nubmerStrs[a], String.valueOf(a));
+    }
+
+    return Long.parseLong(numbers);
   }
 }
