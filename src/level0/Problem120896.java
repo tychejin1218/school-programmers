06116e994039e55a56eed2ffb7a93c7c5ac79dd1
너나들이 <tychejin@naver.com>
2023-05-03 10:07:25 +0900

    Chore: 코딩테스트 연습
    문제_120895 : 인덱스 바꾸기

--- src/level0/Problem120896.java
+++ src/level0/Problem120896.java
@@ -1,5 +1,12 @@
 package level0;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * 한 번만 등장한 문자
  * <p/>
@@ -7,8 +14,31 @@ package level0;
  */
 public class Problem120896 {
 
+  public String solution01(String s) {
+
+    List<String> strList = Arrays.stream(s.split(""))
+        .collect(Collectors.toList());
+    Collections.sort(strList);
+
+    Set<String> strSet = strList.stream()
+        .filter(p -> Collections.frequency(strList, p) > 1)
+        .collect(Collectors.toSet());
+    for (String dStr : strSet) {
+      strList.removeIf(str -> str.equals(dStr));
+    }
+
+    return strList.stream()
+        .collect(Collectors.joining());
+  }
+
   public String solution(String s) {
-    String answer = "";
-    return answer;
+    return Arrays.stream(s.split(""))
+        .collect(Collectors.groupingBy(s1 -> s1))
+        .entrySet()
+        .stream()
+        .filter(entry -> entry.getValue().size() <= 1)
+        .map(Map.Entry::getKey)
+        .sorted()
+        .collect(Collectors.joining());
   }
 }
