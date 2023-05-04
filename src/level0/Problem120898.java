dec42fedabac0d7832b9f9415132b1658ba368d5
너나들이 <tychejin@naver.com>
2023-05-04 09:07:08 +0900

    Chore: 코딩테스트 연습
    문제_120898 : 편지

---
+++ src/level0/Problem120898.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 편지
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120898
+ */
+public class Problem120898 {
+
+  public int solution(String message) {
+    int answer = 0;
+    return answer;
+  }
+}
