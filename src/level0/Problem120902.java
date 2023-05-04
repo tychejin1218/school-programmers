00b5dc2d2160b2a28aa35f3653f92906f137040f
너나들이 <tychejin@naver.com>
2023-05-04 12:46:14 +0900

    Chore: 코딩테스트 연습
    문제_120902 : 문자열 계산하기

---
+++ src/level0/Problem120902.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 문자열 계산하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120902
+ */
+public class Problem120902 {
+
+  public int solution(String my_string) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem120902Test.java
@@ -0,0 +1,22 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120902Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String my_string = "3 + 4";
+    int expected = 7;
+
+    // When
+    int result = new Problem120902().solution(my_string);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
