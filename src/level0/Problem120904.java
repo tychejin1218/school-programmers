28909dfb6a50b64a1043e1d69682faf54c6dde3c
너나들이 <tychejin@naver.com>
2023-05-07 20:31:03 +0900

    Chore: 코딩테스트 연습
    문제_120904 : 숫자 찾기

--- src/level0/Problem120904.java
+++ src/level0/Problem120904.java
@@ -7,8 +7,12 @@ package level0;
  */
 public class Problem120904 {
 
-  public int solution(int num, int k) {
+  public int solution01(int num, int k) {
     int answer = String.valueOf(num).indexOf(String.valueOf(k));
     return answer == -1 ? answer : answer + 1;
+  }
+
+  public int solution(int num, int k) {
+    return ("0" + num).indexOf(String.valueOf(k));
   }
 }
