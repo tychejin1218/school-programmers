1de7115bf5d02d13ec249aed0ecbbe4c3e59ad59
너나들이 <tychejin@naver.com>
2023-05-07 20:43:39 +0900

    Chore: 코딩테스트 연습
    문제_120906 : 자릿수 더하기

---
+++ src/level0/Problem120906.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 자릿수 더하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120906
+ */
+public class Problem120906 {
+
+  public int solution(int n) {
+    int answer = 0;
+    return answer;
+  }
+}
