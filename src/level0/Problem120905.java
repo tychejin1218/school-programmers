7465fe75b39fd15264a3f7e8059b870c28907cb0
너나들이 <tychejin@naver.com>
2023-05-07 20:34:48 +0900

    Chore: 코딩테스트 연습
    문제_120905 : n의 배수 고르기

---
+++ src/level0/Problem120905.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * n의 배수 고르기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120905
+ */
+public class Problem120905 {
+
+  public int[] solution(int n, int[] numlist) {
+    int[] answer = {};
+    return answer;
+  }
+}
