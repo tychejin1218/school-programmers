29284b20f74f66ddaa3db7d6bf7524aa2d9a1ad7
너나들이 <tychejin@naver.com>
2023-05-08 09:00:19 +0900

    Chore: 코딩테스트 연습
    문제_120907 : OX퀴즈

---
+++ src/level0/Problem120907.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * OX퀴즈
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120907
+ */
+public class Problem120907 {
+
+  public String[] solution(String[] quiz) {
+    String[] answer = {};
+    return answer;
+  }
+}
---
+++ test/level0/Problem120907Test.java
@@ -0,0 +1,36 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120907Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String[] quiz = {"3 - 4 = -3", "5 + 6 = 11"};
+    String[] expected = {"X", "O"};
+
+    // When
+    String[] result = new Problem120907().solution(quiz);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String[] quiz = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
+    String[] expected = {"O", "O", "X", "O"};
+
+    // When
+    String[] result = new Problem120907().solution(quiz);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
