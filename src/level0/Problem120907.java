362dbfdd216592938d7e4d8e23dd44135123627e
너나들이 <tychejin@naver.com>
2023-05-08 09:12:59 +0900

    Chore: 코딩테스트 연습
    문제_120907 : OX퀴즈

--- src/level0/Problem120907.java
+++ src/level0/Problem120907.java
@@ -1,5 +1,8 @@
 package level0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * OX퀴즈
  * <p/>
@@ -8,7 +11,25 @@ package level0;
 public class Problem120907 {
 
   public String[] solution(String[] quiz) {
-    String[] answer = {};
-    return answer;
+
+    List<String> answerList = new ArrayList<>();
+    for (String quizStr : quiz) {
+      String[] quizArr = quizStr.split(" ");
+
+      int expected;
+      if ("+".equals(quizArr[1])) {
+        expected = Integer.parseInt(quizArr[0]) + Integer.parseInt(quizArr[2]);
+      } else {
+        expected = Integer.parseInt(quizArr[0]) - Integer.parseInt(quizArr[2]);
+      }
+
+      if (expected == Integer.parseInt(quizArr[4])) {
+        answerList.add("O");
+      } else {
+        answerList.add("X");
+      }
+    }
+
+    return answerList.toArray(new String[answerList.size()]);
   }
 }
