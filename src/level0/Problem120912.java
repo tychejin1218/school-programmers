fb47dd34d78a0f4032791e2483f89c7121334061
너나들이 <tychejin@naver.com>
2023-05-09 09:30:14 +0900

    Chore: 코딩테스트 연습
    문제_120912 : 7의 개수

---
+++ src/level0/Problem120912.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 7의 개수
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120912
+ */
+public class Problem120912 {
+
+  public int solution(int[] array) {
+    int answer = 0;
+    return answer;
+  }
+}
