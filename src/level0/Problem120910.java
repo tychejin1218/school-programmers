a3a72bbbdb5639e5059846d4bb60d38d40340199
너나들이 <tychejin@naver.com>
2023-05-09 09:04:21 +0900

    Chore: 코딩테스트 연습
    문제_120910 : 세균 증식

---
+++ src/level0/Problem120910.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 세균 증식
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120910
+ */
+public class Problem120910 {
+
+  public int solution(int n, int t) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem120910Test.java
@@ -0,0 +1,38 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120910Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int n = 2;
+    int t = 10;
+    int expected = 2048;
+
+    // When
+    int result = new Problem120910().solution(n, t);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int n = 7;
+    int t = 15;
+    int expected = 229376;
+
+    // When
+    int result = new Problem120910().solution(n, t);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
