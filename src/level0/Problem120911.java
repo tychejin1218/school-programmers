fef18d4e87766e556dd8190a7560cab972a2c6b1
너나들이 <tychejin@naver.com>
2023-05-09 09:26:44 +0900

    Chore: 코딩테스트 연습
    문제_120911 : 문자열 정렬하기 (2)

--- src/level0/Problem120911.java
+++ src/level0/Problem120911.java
@@ -1,5 +1,8 @@
 package level0;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * 문자열 정렬하기 (2)
  * <p/>
@@ -8,7 +11,8 @@ package level0;
 public class Problem120911 {
 
   public String solution(String my_string) {
-    String answer = "";
-    return answer;
+    return Arrays.stream(my_string.toLowerCase().split(""))
+        .sorted()
+        .collect(Collectors.joining());
   }
 }
