96aa8260b86da066e8ec13fd2230a4b2e9860ac9
너나들이 <tychejin@naver.com>
2023-05-10 10:18:55 +0900

    Chore: 코딩테스트 연습
    문제_120913 : 잘라서 배열로 저장하기

--- src/level0/Problem120913.java
+++ src/level0/Problem120913.java
@@ -1,5 +1,8 @@
 package level0;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 잘라서 배열로 저장하기
  * <p/>
@@ -8,7 +11,17 @@ package level0;
 public class Problem120913 {
 
   public String[] solution(String my_str, int n) {
-    String[] answer = {};
-    return answer;
+    List<String> strList = new ArrayList<>();
+    int myStrLen = my_str.length();
+    for (int a = 0; a < myStrLen; a += n) {
+      int beginIndex = a;
+      int endIndex = a + n;
+      if (myStrLen < endIndex) {
+        strList.add(my_str.substring(beginIndex, myStrLen));
+      } else {
+        strList.add(my_str.substring(beginIndex, endIndex));
+      }
+    }
+    return strList.toArray(String[]::new);
   }
 }
