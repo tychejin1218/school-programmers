f20ddffe5cdf26c0abc6dd05007faaf6800c911d
너나들이 <tychejin@naver.com>
2023-05-10 09:17:43 +0900

    Chore: 코딩테스트 연습
    문제_120912 : 7의 개수

--- src/level0/Problem120912.java
+++ src/level0/Problem120912.java
@@ -1,6 +1,7 @@
 package level0;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -10,12 +11,22 @@ import java.util.stream.IntStream;
  */
 public class Problem120912 {
 
-  public int solution(int[] array) {
+  public int solution01(int[] array) {
     return IntStream.range(0, array.length)
         .mapToObj(index -> (int) Arrays.stream(String.valueOf(array[index]).split(""))
             .filter(value -> value.equals("7"))
             .count())
         .reduce(0, Integer::sum);
   }
-  
+
+  public int solution(int[] array) {
+    return (int) Arrays.stream(
+            Arrays.stream(array)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining())
+                .split("")
+        )
+        .filter(s -> s.equals("7"))
+        .count();
+  }
 }
