ef351cb1abe1620427e7266e58f65aefab1b9520
너나들이 <tychejin@naver.com>
2023-05-11 09:28:16 +0900

    Chore: 코딩테스트 연습
    문제_120921 : 문자열 밀기

---
+++ src/level0/Problem120921.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 문자열 밀기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120921
+ */
+public class Problem120921 {
+
+  public int solution(String A, String B) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem120921Test.java
@@ -0,0 +1,68 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem120921Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String A = "hello";
+    String B = "ohell";
+    int expected = 1;
+
+    // When
+    int result = new Problem120921().solution(A, B);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String A = "apple";
+    String B = "elppa";
+    int expected = -1;
+
+    // When
+    int result = new Problem120921().solution(A, B);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    String A = "atat";
+    String B = "tata";
+    int expected = 1;
+
+    // When
+    int result = new Problem120921().solution(A, B);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution04() {
+
+    // Given
+    String A = "abc";
+    String B = "abc";
+    int expected = 0;
+
+    // When
+    int result = new Problem120921().solution(A, B);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
