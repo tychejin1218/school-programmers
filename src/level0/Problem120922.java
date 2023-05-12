211d1e74336cab1400633be2f3c6928e208049e3
너나들이 <tychejin@naver.com>
2023-05-12 09:42:38 +0900

    Chore: 코딩테스트 연습
    문제_120922 : 종이 자르기

---
+++ src/level0/Problem120922.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 종이 자르기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120922
+ */
+public class Problem120922 {
+
+  public int solution(int M, int N) {
+    int answer = 0;
+    return answer;
+  }
+}
