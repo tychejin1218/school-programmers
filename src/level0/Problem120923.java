12250c75715ee75ea47d491df9b6e6b0ad426302
너나들이 <tychejin@naver.com>
2023-05-13 10:27:57 +0900

    Chore: 코딩테스트 연습
    문제_120923 : 연속된 수의 합

---
+++ src/level0/Problem120923.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 연속된 수의 합
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120923
+ */
+public class Problem120923 {
+
+  public int[] solution(int num, int total) {
+    int[] answer = {};
+    return answer;
+  }
+}
