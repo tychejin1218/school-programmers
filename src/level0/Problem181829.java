3a5f4e62080c9981a5c88c111be292605a609287
너나들이 <tychejin@naver.com>
2023-05-18 08:59:37 +0900

    Chore: 코딩테스트 연습
    문제_181829 : 이차원 배열 대각선 순회하기

---
+++ src/level0/Problem181829.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 이차원 배열 대각선 순회하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181829
+ */
+public class Problem181829 {
+
+  public int solution(int[][] board, int k) {
+    int answer = 0;
+    return answer;
+  }
+}
---
+++ test/level0/Problem181829Test.java
@@ -0,0 +1,23 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181829Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[][] board = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
+    int k = 2;
+    int expected = 8;
+
+    // When
+    int result = new Problem181829().solution(board, k);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
