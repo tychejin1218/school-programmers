ad3000d7976f9ee52ee7de04eab7a7ccf84dfa70
너나들이 <tychejin@naver.com>
2023-05-17 09:04:24 +0900

    Chore: 코딩테스트 연습
    문제_120956 : 옹알이 (1)

---
+++ src/level0/Problem120956.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 옹알이 (1)
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120956
+ */
+public class Problem120956 {
+
+  public int solution(String[] babbling) {
+    int answer = 0;
+    return answer;
+  }
+}
