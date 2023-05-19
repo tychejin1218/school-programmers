c6f797414decae32b3373235aa1e865d04ec7b06
너나들이 <tychejin@naver.com>
2023-05-19 12:52:44 +0900

    Chore: 코딩테스트 연습
    문제_181830 : 정사각형으로 만들기

---
+++ src/level0/Problem181830.java
@@ -0,0 +1,14 @@
+package level0;
+
+/**
+ * 정사각형으로 만들기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181830
+ */
+public class Problem181830 {
+
+  public int[][] solution(int[][] arr) {
+    int[][] answer = {};
+    return answer;
+  }
+}
