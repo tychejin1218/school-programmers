d401b8c67b95cbf993faa94ff8bb038f9e0542d2
너나들이 <tychejin@naver.com>
2023-05-30 09:11:23 +0900

    Chore: 코딩테스트 연습
    문제_181830 : 정사각형으로 만들기

--- src/level0/Problem181830.java
+++ src/level0/Problem181830.java
@@ -9,6 +9,39 @@ public class Problem181830 {
 
   public int[][] solution(int[][] arr) {
     int[][] answer = {};
+
+    int rowLen = arr.length;
+    int colLen = arr[0].length;
+
+    System.out.println("rowLen : " + rowLen);
+    System.out.println("colLen : " + colLen);
+
+    if (rowLen == colLen) {
+      answer = arr;
+    } else if (rowLen > colLen) {
+      answer = new int[rowLen][rowLen];
+      for (int a = 0; a < rowLen; a++) {
+        for (int b = 0; b < colLen; b++) {
+          if (b >= colLen) {
+            answer[a][b] = 0;
+          } else {
+            answer[a][b] = arr[a][b];
+          }
+        }
+      }
+    } else if (rowLen < colLen) {
+      answer = new int[colLen][colLen];
+      for (int a = 0; a < colLen; a++) {
+        for (int b = 0; b < colLen; b++) {
+          if (a >= rowLen) {
+            answer[a][b] = 0;
+          } else {
+            answer[a][b] = arr[a][b];
+          }
+        }
+      }
+    }
+
     return answer;
   }
 }
