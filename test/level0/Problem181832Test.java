d1edf2823d97af56d308356adafdb7839474e705
너나들이 <tychejin@naver.com>
2023-07-14 16:44:49 +0900

    Chore: 코딩테스트 연습
    문제_181832 : 정수를 나선형으로 배치하기

---
+++ test/level0/Problem181832Test.java
@@ -0,0 +1,37 @@
+package level0;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181832Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int n = 4;
+    int[][] expected = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
+
+    // When
+    int[][] result = new Problem181832().solution(n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int n = 5;
+    int[][] expected = {{1, 2, 3, 4, 5}, {16, 17, 18, 19, 6}, {15, 24, 25, 20, 7},
+        {14, 23, 22, 21, 8}, {13, 12, 11, 10, 9}};
+
+    // When
+    int[][] result = new Problem181832().solution(n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
