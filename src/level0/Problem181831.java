67a8c355e43adb89b7bcebc26efab1f9dd3e32c2
너나들이 <tychejin@naver.com>
2023-07-14 12:28:34 +0900

    Chore: 코딩테스트 연습
    문제_181831 : 특별한 이차원 배열 2

--- src/level0/Problem181831.java
+++ src/level0/Problem181831.java
@@ -1,5 +1,7 @@
 package level0;
 
+import java.util.stream.IntStream;
+
 /**
  * 특별한 이차원 배열 2
  * <p/>
@@ -7,13 +9,13 @@ package level0;
  */
 public class Problem181831 {
 
-  public int solution(int[][] arr) {
+  public int solution01(int[][] arr) {
     int answer = 1;
 
     int n = arr.length;
     for (int a = 0; a < n; a++) {
       for (int b = 0; b < n; b++) {
-        if(arr[a][b] != arr[b][a]){
+        if (arr[a][b] != arr[b][a]) {
           answer = 0;
           break;
         }
@@ -21,5 +23,14 @@ public class Problem181831 {
     }
 
     return answer;
+  }
+
+  public int solution(int[][] arr) {
+    int n = arr.length;
+    return IntStream.range(0, n)
+        .allMatch(a ->
+            IntStream.range(0, n)
+                .allMatch(b -> arr[a][b] == arr[b][a])
+        ) ? 1 : 0;
   }
 }
