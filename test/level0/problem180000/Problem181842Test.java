bf2abc5a95bbbed7ef167f00f87dbe30c7a19067
너나들이 <tychejin@naver.com>
2024-04-02 09:03:12 +0900

    Chore: 코딩테스트 연습
    문제_181842 : 부분 문자열

---
+++ test/level0/problem180000/Problem181842Test.java
@@ -0,0 +1,38 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181842Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String str1 = "abc";
+    String str2 = "aabcc";
+    int expected = 1;
+
+    // When
+    int result = new Problem181842().solution(str1, str2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String str1 = "tbt";
+    String str2 = "tbbttb";
+    int expected = 0;
+
+    // When
+    int result = new Problem181842().solution(str1, str2);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
