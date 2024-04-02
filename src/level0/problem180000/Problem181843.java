a3148b1f6b325f03eb33791cac9ce6df4d1c1128
너나들이 <tychejin@naver.com>
2024-04-02 09:07:33 +0900

    Chore: 코딩테스트 연습
    문제_181843 : 부분 문자열인지 확인하기

---
+++ src/level0/problem180000/Problem181843.java
@@ -0,0 +1,13 @@
+package level0.problem180000;
+
+/**
+ * 부분 문자열인지 확인하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181843
+ */
+public class Problem181843 {
+
+  public int solution(String my_string, String target) {
+    return my_string.contains(target) ? 1 : 0;
+  }
+}
