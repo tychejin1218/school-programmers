73f0ec21cd07543ade77a70a1df3ca9397e170a1
너나들이 <tychejin@naver.com>
2024-04-03 11:03:34 +0900

    Chore: 코딩테스트 연습
    문제_181846 : 두 수의 합

---
+++ src/level0/problem180000/Problem181846.java
@@ -0,0 +1,15 @@
+package level0.problem180000;
+
+import java.math.BigInteger;
+
+/**
+ * 두 수의 합
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181846
+ */
+public class Problem181846 {
+
+  public String solution(String a, String b) {
+    return String.valueOf(new BigInteger(a).add(new BigInteger(b)));
+  }
+}
