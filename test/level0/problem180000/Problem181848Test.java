cabce9902dd3ae7ee80904c52fd51dbcfdb114d8
너나들이 <tychejin@naver.com>
2024-04-03 11:13:55 +0900

    Chore: 코딩테스트 연습
    문제_181848 : 문자열을 정수로 변환하기

---
+++ test/level0/problem180000/Problem181848Test.java
@@ -0,0 +1,36 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181848Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String n_str = "10";
+    int expected = 10;
+
+    // When
+    int result = new Problem181848().solution(n_str);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String n_str = "8542";
+    int expected = 8542;
+
+    // When
+    int result = new Problem181848().solution(n_str);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
