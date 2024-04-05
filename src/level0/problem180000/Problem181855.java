e8c8fbda2428a13fe4cb2a6ca9f373631e8ae30b
너나들이 <tychejin@naver.com>
2024-04-05 13:54:36 +0900

    Chore: 코딩테스트 연습
    문제_181855 : 문자열 묶기

---
+++ src/level0/problem180000/Problem181855.java
@@ -0,0 +1,57 @@
+package level0.problem180000;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 문자열 묶기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181855
+ */
+public class Problem181855 {
+
+  public int solution01(String[] strArr) {
+
+    int[] lenArr = IntStream.range(0, strArr.length)
+        .map(i -> strArr[i].length())
+        .toArray();
+
+    Map<Integer, Integer> lenMap = new HashMap<>();
+    for (int i = 0; i < lenArr.length; i++) {
+      int key = lenArr[i];
+      int value = lenMap.get(key) == null ? 0 : lenMap.get(key);
+      lenMap.put(key, value + 1);
+    }
+
+    return lenMap.entrySet().stream()
+        .mapToInt(Map.Entry::getValue)
+        .max()
+        .getAsInt();
+  }
+
+  public int solution02(String[] strArr) {
+
+    Map<Integer, Integer> strMap = new HashMap<>();
+
+    for (String str : strArr) {
+      int strLen = str.length();
+      strMap.put(strLen, strMap.getOrDefault(strLen, 0) + 1);
+    }
+
+    return Collections.max(strMap.values());
+  }
+
+  public int solution(String[] strArr) {
+    return Arrays.stream(strArr)
+        .collect(Collectors.groupingBy(String::length, Collectors.counting()))
+        .values()
+        .stream()
+        .mapToInt(Long::intValue)
+        .max()
+        .orElse(0);
+  }
+}
---
+++ test/level0/problem180000/Problem181855Test.java
@@ -0,0 +1,22 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181855Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String[] strArr = {"a", "bc", "d", "efg", "hi"};
+    int expected = 2;
+
+    // When
+    int result = new Problem181855().solution(strArr);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
