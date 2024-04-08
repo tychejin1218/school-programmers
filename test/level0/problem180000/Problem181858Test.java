dc6be2c279329344b224e7ce46c6e661643261a7
너나들이 <tychejin@naver.com>
2024-04-08 16:57:22 +0900

    Chore: 코딩테스트 연습
    문제_181858 : 무작위로 K개의 수 뽑기

---
+++ test/level0/problem180000/Problem181858Test.java
@@ -0,0 +1,38 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181858Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] arr = {0, 1, 1, 2, 2, 3};
+    int k = 3;
+    int[] expected = {0, 1, 2};
+
+    // When
+    int[] result = new Problem181858().solution(arr, k);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] arr = {0, 1, 1, 1, 1};
+    int k = 4;
+    int[] expected = {0, 1, -1, -1};
+
+    // When
+    int[] result = new Problem181858().solution(arr, k);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
