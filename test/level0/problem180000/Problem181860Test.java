ba0ad5b101ce14e2dce6b4283b773fdfe78030f2
너나들이 <tychejin@naver.com>
2024-04-09 09:54:45 +0900

    Chore: 코딩테스트 연습
    문제_181860 : 빈 배열에 추가, 삭제하기

---
+++ test/level0/problem180000/Problem181860Test.java
@@ -0,0 +1,23 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181860Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] arr = {3, 2, 4, 1, 3};
+    boolean[] flag = {true, false, true, false, false};
+    int[] expected = {3, 3, 3, 3, 4, 4, 4, 4};
+
+    // When
+    int[] result = new Problem181860().solution(arr, flag);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
