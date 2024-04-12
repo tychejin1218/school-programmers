75e5a6a184788ba903083cd3b3654b6ee9b538a7
너나들이 <tychejin@naver.com>
2024-04-12 13:30:58 +0900

    Chore: 코딩테스트 연습
    문제_181865 : 간단한 식 계산하기

---
+++ test/level0/problem180000/Problem181865Test.java
@@ -0,0 +1,50 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181865Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String binomial = "43 + 12";
+    int expected = 55;
+
+    // When
+    int result = new Problem181865().solution(binomial);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String binomial = "0 - 7777";
+    int expected = -7777;
+
+    // When
+    int result = new Problem181865().solution(binomial);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    String binomial = "40000 * 40000";
+    int expected = 1600000000;
+
+    // When
+    int result = new Problem181865().solution(binomial);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
