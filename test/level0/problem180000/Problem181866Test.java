acb57133278ba73f76ff5f9fca5d0b5b43d7d265
너나들이 <tychejin@naver.com>
2024-04-12 14:00:38 +0900

    Chore: 코딩테스트 연습
    문제_181866 : 문자열 잘라서 정렬하기

---
+++ test/level0/problem180000/Problem181866Test.java
@@ -0,0 +1,36 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181866Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String myString = "axbxcxdx";
+    String[] expected = {"a", "b", "c", "d"};
+
+    // When
+    String[] result = new Problem181866().solution(myString);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String myString = "dxccxbbbxaaaa";
+    String[] expected = {"aaaa","bbb","cc","d"};
+
+    // When
+    String[] result = new Problem181866().solution(myString);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
