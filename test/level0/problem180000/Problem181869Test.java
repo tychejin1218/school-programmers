efdc538c27113f110285b14b20b0027e2a3a8c36
너나들이 <tychejin@naver.com>
2024-04-18 13:09:48 +0900

    Chore: 코딩테스트 연습
    문제_181869 : 공백으로 구분하기 1

---
+++ test/level0/problem180000/Problem181869Test.java
@@ -0,0 +1,36 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181869Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String myString = "i love you";
+    String[] expected = {"i", "love", "you"};
+
+    // When
+    String[] result = new Problem181869().solution(myString);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String myString = "programmers";
+    String[] expected = {"programmers"};
+
+    // When
+    String[] result = new Problem181869().solution(myString);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
