ddbf38083ef1117cd74360372e39bcc6e9829b85
너나들이 <tychejin@naver.com>
2024-04-23 13:12:07 +0900

    Chore: 코딩테스트 연습
    문제_181876 : 소문자로 바꾸기

---
+++ src/level0/problem180000/Problem181876.java
@@ -0,0 +1,13 @@
+package level0.problem180000;
+
+/**
+ * 소문자로 바꾸기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181876
+ */
+public class Problem181876 {
+
+  public String solution(String myString) {
+    return myString.toLowerCase();
+  }
+}
---
+++ test/level0/problem180000/Problem181876Test.java
@@ -0,0 +1,36 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181876Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String myString = "aBcDeFg";
+    String expected = "abcdefg";
+
+    // When
+    String result = new Problem181876().solution(myString);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String myString = "aaa";
+    String expected = "aaa";
+
+    // When
+    String result = new Problem181876().solution(myString);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
