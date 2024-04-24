4cb7e1c0a75c73909aa4aa153f49552009b95a5f
너나들이 <tychejin@naver.com>
2024-04-24 09:58:10 +0900

    Chore: 코딩테스트 연습
    문제_181880 : 1로 만들기

---
+++ test/level0/problem180000/Problem181880Test.java
@@ -0,0 +1,22 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181880Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] num_list = {12, 4, 15, 1, 14};
+    int expected = 11;
+
+    // When
+    int result = new Problem181880().solution(num_list);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
