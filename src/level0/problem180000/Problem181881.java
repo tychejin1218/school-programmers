96aceeee5617ee20d563eda7415ea6d824290a4c
너나들이 <tychejin@naver.com>
2024-04-25 13:21:11 +0900

    Chore: 코딩테스트 연습
    문제_181881 : 조건에 맞게 수열 변환하기 2

--- src/level0/problem180000/Problem181881.java
+++ src/level0/problem180000/Problem181881.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class Problem181881 {
 
-  public int solution(int[] arr) {
+  public int solution01(int[] arr) {
     int answer = 0;
 
     int arrLen = arr.length;
@@ -32,6 +32,35 @@ public class Problem181881 {
           .toArray();
       previousArr = Arrays.copyOf(currentArr, arrLen);
       currentArr = Arrays.copyOf(tempArr, arrLen);
+      answer++;
+    }
+
+    return answer - 1;
+  }
+
+  public int solution(int[] arr) {
+    int answer = 0;
+
+    int arrLen = arr.length;
+    int[] previousArr = new int[arrLen];
+    int[] currentArr = arr.clone();
+
+    while (!Arrays.equals(previousArr, currentArr)) {
+      int[] tempArr = Arrays.stream(currentArr)
+          .map(i -> {
+            int result = 0;
+            if (i >= 50 && i % 2 == 0) {
+              result = i / 2;
+            } else if (i < 50 && i % 2 != 0) {
+              result = i * 2 + 1;
+            } else {
+              result = i;
+            }
+            return result;
+          })
+          .toArray();
+      previousArr = currentArr.clone();
+      currentArr = tempArr.clone();
       answer++;
     }
 
