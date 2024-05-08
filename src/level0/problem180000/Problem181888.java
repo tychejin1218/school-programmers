d061bda770a840271477cf5ab4b1756985f557fe
너나들이 <tychejin@naver.com>
2024-05-08 09:22:28 +0900

    Chore: 코딩테스트 연습
    문제_181888 : n개 간격의 원소들

---
+++ src/level0/problem180000/Problem181888.java
@@ -0,0 +1,18 @@
+package level0.problem180000;
+
+import java.util.stream.IntStream;
+
+/**
+ * n개 간격의 원소들
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181888
+ */
+public class Problem181888 {
+
+  public int[] solution(int[] num_list, int n) {
+    return IntStream.range(0, num_list.length)
+        .filter(i -> i % n ==0)
+        .map(i -> num_list[i])
+        .toArray();
+  }
+}
---
+++ test/level0/problem180000/Problem181888Test.java
@@ -0,0 +1,38 @@
+package level0.problem180000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181888Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] num_list = {4, 2, 6, 1, 7, 6};
+    int n = 2;
+    int[] expected = {4, 6, 7};
+
+    // When
+    int[] result = new Problem181888().solution(num_list, n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] num_list = {4, 2, 6, 1, 7, 6};
+    int n = 4;
+    int[] expected = {4, 7};
+
+    // When
+    int[] result = new Problem181888().solution(num_list, n);
+
+    // Then
+    assertArrayEquals(expected, result);
+  }
+}
