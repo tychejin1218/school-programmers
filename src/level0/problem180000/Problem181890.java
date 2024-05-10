dc3d8e619065fc2719086ffa4071a15f7048baff
너나들이 <tychejin@naver.com>
2024-05-10 09:28:47 +0900

    Chore: 코딩테스트 연습
    문제_181890 : 왼쪽 오른쪽

--- src/level0/problem180000/Problem181890.java
+++ src/level0/problem180000/Problem181890.java
@@ -1,5 +1,6 @@
 package level0.problem180000;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 /**
@@ -10,20 +11,13 @@ import java.util.stream.IntStream;
 public class Problem181890 {
 
   public String[] solution(String[] str_list) {
-
     for (int i = 0; i < str_list.length; i++) {
-
       if (str_list[i].equals("l")) {
-        String[] answer = new String[i];
-        System.arraycopy(str_list, 0, answer, 0, i);
-        return answer;
+        return Arrays.copyOfRange(str_list, 0, i);
       } else if (str_list[i].equals("r")) {
-        String[] answer = new String[str_list.length - i - 1];
-        System.arraycopy(str_list, i + 1, answer, 0, str_list.length - i - 1);
-        return answer;
+        return Arrays.copyOfRange(str_list, i, str_list.length);
       }
     }
     return new String[]{};
   }
-
 }
