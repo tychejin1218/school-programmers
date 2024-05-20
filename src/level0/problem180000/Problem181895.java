bf01765dc8b3b953356ce1d4d9710c866ecb721b
너나들이 <tychejin@naver.com>
2024-05-20 09:05:46 +0900

    Chore: 코딩테스트 연습
    문제_181895 : 배열 만들기 3

---
+++ src/level0/problem180000/Problem181895.java
@@ -0,0 +1,17 @@
+package level0.problem180000;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * 배열 만들기 3
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181895
+ */
+public class Problem181895 {
+
+  public int[] solution(int[] arr, int[][] intervals) {
+    int[] answer = {};
+    return answer;
+  }
+}
