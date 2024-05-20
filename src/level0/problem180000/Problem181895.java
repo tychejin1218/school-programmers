1a90223b9ddf3503dcd3b34297c39bf9a997552e
너나들이 <tychejin@naver.com>
2024-05-20 09:14:03 +0900

    Chore: 코딩테스트 연습
    문제_181895 : 배열 만들기 3

--- src/level0/problem180000/Problem181895.java
+++ src/level0/problem180000/Problem181895.java
@@ -11,7 +11,9 @@ import java.util.stream.IntStream;
 public class Problem181895 {
 
   public int[] solution(int[] arr, int[][] intervals) {
-    int[] answer = {};
-    return answer;
+    return IntStream.concat(
+        Arrays.stream(Arrays.copyOfRange(arr, intervals[0][0], intervals[0][1] + 1)),
+        Arrays.stream(Arrays.copyOfRange(arr, intervals[1][0], intervals[1][1] + 1))
+    ).toArray();
   }
 }
