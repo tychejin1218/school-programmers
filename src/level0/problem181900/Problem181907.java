6a5297cdd109d864dc02743a7084193626dbc865
너나들이 <tychejin@naver.com>
2024-06-27 17:58:00 +0900

    Chore: 코딩테스트 연습
    문제_1819007 : 문자열의 앞의 n글자

---
+++ src/level0/problem181900/Problem181907.java
@@ -0,0 +1,13 @@
+package level0.problem181900;
+
+/**
+ * 문자열의 앞의 n글자
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181906
+ */
+public class Problem181907 {
+
+  public String solution(String my_string, int n) {
+    return my_string.substring(0, n);
+  }
+}
