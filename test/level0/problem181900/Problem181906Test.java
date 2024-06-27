eb71e68573ba4997d25147bab1485941942d146f
너나들이 <tychejin@naver.com>
2024-06-27 17:54:52 +0900

    Chore: 코딩테스트 연습
    문제_1819006 : 접두사인지 확인하기

---
+++ test/level0/problem181900/Problem181906Test.java
@@ -0,0 +1,68 @@
+package level0.problem181900;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181906Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String my_string = "banana";
+    String is_prefix = "ban";
+    int expected = 1;
+
+    // When
+    int result = new Problem181906().solution(my_string, is_prefix);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String my_string = "banana";
+    String is_prefix = "nan";
+    int expected = 0;
+
+    // When
+    int result = new Problem181906().solution(my_string, is_prefix);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    String my_string = "banana";
+    String is_prefix = "abcd";
+    int expected = 0;
+
+    // When
+    int result = new Problem181906().solution(my_string, is_prefix);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution04() {
+
+    // Given
+    String my_string = "banana";
+    String is_prefix = "bananan";
+    int expected = 0;
+
+    // When
+    int result = new Problem181906().solution(my_string, is_prefix);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
