302944854bf6d7ee88447c6ba3780e641816faec
너나들이 <tychejin@naver.com>
2024-07-02 09:55:29 +0900

    Chore: 코딩테스트 연습
    문제_1819013 : 문자열 여러 번 뒤집기

---
+++ src/level0/problem181900/Problem181913.java
@@ -0,0 +1,24 @@
+package level0.problem181900;
+
+/**
+ * 문자열 여러 번 뒤집기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181913
+ */
+public class Problem181913 {
+
+  public String solution(String my_string, int[][] queries) {
+
+    StringBuilder stringBuilder = new StringBuilder(my_string);
+
+    for (int[] query : queries) {
+      int s = query[0];
+      int e = query[1];
+      stringBuilder.replace(s, e + 1,
+          new StringBuilder(my_string.substring(s, e + 1)).reverse().toString());
+      my_string = stringBuilder.toString();
+    }
+
+    return my_string;
+  }
+}
