b510c6c4490edb266e52abe2ec01a5ab09f2df4d
너나들이 <tychejin@naver.com>
2024-07-09 14:53:49 +0900

    Chore: 코딩테스트 연습
    문제_1819021 : 배열 만들기 2

---
+++ src/level0/problem181900/Problem181921.java
@@ -0,0 +1,32 @@
+package level0.problem181900;
+
+import java.util.stream.IntStream;
+
+/**
+ * 카운트 업
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181920
+ */
+public class Problem181921 {
+
+  public int[] solution01(int l, int r) {
+    return IntStream.rangeClosed(l, r)
+        .filter(i -> i % 5 == 0
+            && !(String.valueOf(i).contains("1")
+            || String.valueOf(i).contains("2")
+            || String.valueOf(i).contains("3")
+            || String.valueOf(i).contains("4")
+            || String.valueOf(i).contains("6")
+            || String.valueOf(i).contains("7")
+            || String.valueOf(i).contains("8")
+            || String.valueOf(i).contains("9")
+        ))
+        .toArray();
+  }
+
+  public int[] solution(int l, int r) {
+    return IntStream.rangeClosed(l, r)
+        .filter(i -> i % 5 == 0 && String.valueOf(i).matches("[50]*"))
+        .toArray();
+  }
+}
