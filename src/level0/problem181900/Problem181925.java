c8610c9f7ae4d4eb86bdfb2c8a43085853117c50
너나들이 <tychejin@naver.com>
2024-07-15 13:04:39 +0900

    Chore: 코딩테스트 연습
    문제_181925 : 수 조작하기 2

---
+++ src/level0/problem181900/Problem181925.java
@@ -0,0 +1,31 @@
+package level0.problem181900;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 수 조작하기 2
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181925
+ */
+public class Problem181925 {
+
+  public String solution(int[] numLog) {
+
+    List<String> controlList = new ArrayList<>();
+
+    for (int i = 1; i < numLog.length; i++) {
+      boolean isIncrement = numLog[i] > numLog[i - 1];
+
+      int numDifference = Math.abs(numLog[i] - numLog[i - 1]);
+      if (numDifference == 1) {
+        controlList.add(isIncrement ? "w" : "s");
+      } else if (numDifference == 10) {
+        controlList.add(isIncrement ? "d" : "a");
+      }
+    }
+
+    return controlList.stream().collect(Collectors.joining());
+  }
+}
