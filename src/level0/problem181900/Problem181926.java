b4c6239eb39e3f038442b409c9371affdcdc5454
너나들이 <tychejin@naver.com>
2024-07-15 13:05:00 +0900

    Chore: 코딩테스트 연습
    문제_181926 : 수 조작하기 1

---
+++ src/level0/problem181900/Problem181926.java
@@ -0,0 +1,59 @@
+package level0.problem181900;
+
+import java.util.Arrays;
+
+/**
+ * 수 조작하기 1
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181925
+ */
+public class Problem181926 {
+
+  public int solution(int n, String control) {
+
+    String[] controls = control.split("");
+    for (String controlChar : controls) {
+      switch (controlChar) {
+        case "w":
+          n++;
+          break;
+        case "s":
+          n--;
+          break;
+        case "d":
+          n += 10;
+          break;
+        case "a":
+          n -= 10;
+          break;
+      }
+    }
+
+    return n;
+  }
+
+  public int solution01(int n, String control) {
+
+    int[] nums = new int[1];
+    nums[0] = n;
+    Arrays.stream(control.split(""))
+        .forEach(controlChar -> {
+          switch (controlChar) {
+            case "w":
+              nums[0]++;
+              break;
+            case "s":
+              nums[0]--;
+              break;
+            case "d":
+              nums[0] += 10;
+              break;
+            case "a":
+              nums[0] -= 10;
+              break;
+          }
+        });
+
+    return nums[0];
+  }
+}
