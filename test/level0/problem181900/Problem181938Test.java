0331178b8c64c38bfe6583328ce8e801597eea58
너나들이 <tychejin@naver.com>
2024-07-21 02:10:57 +0900

    Chore: 코딩테스트 연습
    문제_181938 : 두 수의 연산값 비교하기

---
+++ test/level0/problem181900/Problem181938Test.java
@@ -0,0 +1,38 @@
+package level0.problem181900;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181938Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int a = 2;
+    int b = 91;
+    int expected = 364;
+
+    // When
+    int result = new Problem181938().solution(a, b);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int a = 91;
+    int b = 2;
+    int expected = 912;
+
+    // When
+    int result = new Problem181938().solution(a, b);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
