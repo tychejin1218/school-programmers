18d2ce61a0d0ed99010698689decfde383ced5d5
너나들이 <tychejin@naver.com>
2024-07-23 09:47:42 +0900

    Chore: 코딩테스트 연습
    문제_181940 : 문자열 곱하기

---
+++ test/level0/problem181900/Problem181940Test.java
@@ -0,0 +1,38 @@
+package level0.problem181900;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Problem181940Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String my_string = "string";
+    int k = 3;
+    String expected = "stringstringstring";
+
+    // When
+    String result = new Problem181940().solution(my_string, k);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String my_string = "love";
+    int k = 10;
+    String expected = "lovelovelovelovelovelovelovelovelovelove";
+
+    // When
+    String result = new Problem181940().solution(my_string, k);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
