68fa0516200d528ad99f6f503490ae139694443d
너나들이 <tychejin@naver.com>
2024-07-26 09:57:55 +0900

    Chore: 코딩테스트 연습
    문제_181946 : 문자열 붙여서 출력하기

---
+++ src/level0/problem181900/Problem181946.java
@@ -0,0 +1,18 @@
+package level0.problem181900;
+
+import java.util.Scanner;
+
+/**
+ * 문자열 붙여서 출력하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181946
+ */
+public class Problem181946 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    String b = sc.next();
+    System.out.println(a + b);
+  }
+}
