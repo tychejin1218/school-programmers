5e466f50a988af5aeef1e2afb6aec34f395d5f00
너나들이 <tychejin@naver.com>
2024-07-26 10:06:42 +0900

    Chore: 코딩테스트 연습
    문제_181947 : 덧셈식 출력하기

---
+++ src/level0/problem181900/Problem181947.java
@@ -0,0 +1,18 @@
+package level0.problem181900;
+
+import java.util.Scanner;
+
+/**
+ * 덧셈식 출력하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181947
+ */
+public class Problem181947 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(String.format("%d + %d = %d", a, b, (a + b)));
+  }
+}
