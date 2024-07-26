2961a8519b305d4bfa47cf8a6d64a511f67007ba
너나들이 <tychejin@naver.com>
2024-07-26 09:43:28 +0900

    Chore: 코딩테스트 연습
    문제_181944 : 홀짝 구분하기

---
+++ src/level0/problem181900/Problem181944.java
@@ -0,0 +1,18 @@
+package level0.problem181900;
+
+import java.util.Scanner;
+
+/**
+ * 홀짝 구분하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181943
+ */
+public class Problem181944 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    String message = n % 2 == 0 ? n + " is even" : n + " is odd";
+    System.out.println(message);
+  }
+}
