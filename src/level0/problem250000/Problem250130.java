bf3cab8f8d056d8e5e9a0c4789e7830380fb284b
너나들이 <tychejin@naver.com>
2024-07-26 10:55:30 +0900

    Chore: 코딩테스트 연습
    문제_250130 : [PCCE 기출문제] 4번 / 저축

---
+++ src/level0/problem250000/Problem250130.java
@@ -0,0 +1,31 @@
+package level0.problem250000;
+
+import java.util.Scanner;
+
+/**
+ * [PCCE 기출문제] 4번 / 저축
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250130
+ */
+public class Problem250130 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int start = sc.nextInt();
+    int before = sc.nextInt();
+    int after = sc.nextInt();
+
+    int money = start;
+    int month = 1;
+    while (money < 70) {
+      money += before;
+      month++;
+    }
+    while (money < 100) {
+      money += after;
+      month++;
+    }
+
+    System.out.println(month);
+  }
+}
