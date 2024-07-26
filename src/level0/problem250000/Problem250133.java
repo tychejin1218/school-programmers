acdb04e15f1255968d4b1143c3b2a9d663018a47
너나들이 <tychejin@naver.com>
2024-07-26 10:37:35 +0900

    Chore: 코딩테스트 연습
    문제_250133 : [PCCE 기출문제] 1번 / 출력

---
+++ src/level0/problem250000/Problem250133.java
@@ -0,0 +1,21 @@
+package level0.problem250000;
+
+import java.util.Scanner;
+
+/**
+ * [PCCE 기출문제] 1번 / 출력
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250133
+ */
+public class Problem250133 {
+
+  public static void main(String[] args) {
+    String msg = "Spring is beginning";
+    int val1 = 3;
+    String val2 = "3";
+
+    System.out.println(msg);
+    System.out.println(val1 + 10);
+    System.out.println(val2 + "10");
+  }
+}
---
+++ test/level0/problem250000/Problem250133Test.java
@@ -0,0 +1,7 @@
+package level0.problem250000;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Problem250133Test {
+
+}
