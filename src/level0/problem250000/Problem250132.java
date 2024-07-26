495100b099513fbb8a8f256d8e352007cf2489cb
너나들이 <tychejin@naver.com>
2024-07-26 10:43:09 +0900

    Chore: 코딩테스트 연습
    문제_250133 : [PCCE 기출문제] 2번 / 피타고라스의 정리

---
+++ src/level0/problem250000/Problem250132.java
@@ -0,0 +1,23 @@
+package level0.problem250000;
+
+import java.util.Scanner;
+
+/**
+ * [PCCE 기출문제] 2번 / 피타고라스의 정리
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/250132
+ */
+public class Problem250132 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int c = sc.nextInt();
+
+    int aSquare = (int) Math.pow(a, 2);
+    int cSquare = (int) Math.pow(c, 2);
+
+    int b_square = cSquare - aSquare;
+    System.out.println(b_square);
+  }
+}
--- test/level0/problem250000/Problem250133Test.java
+++
@@ -1,7 +0,0 @@
-package level0.problem250000;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class Problem250133Test {
-
-}
