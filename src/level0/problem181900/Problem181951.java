b1e3d63610f8fd0bd6c7ede146fa00556120030b
너나들이 <tychejin@naver.com>
2024-07-26 10:30:30 +0900

    Chore: 코딩테스트 연습
    문제_181951 : a와 b 출력하기

---
+++ src/level0/problem181900/Problem181951.java
@@ -0,0 +1,19 @@
+package level0.problem181900;
+
+import java.util.Scanner;
+
+/**
+ * 문자열 반복해서 출력하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181950
+ */
+public class Problem181951 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(String.format("a = %d", a));
+    System.out.println(String.format("b = %d", b));
+  }
+}
