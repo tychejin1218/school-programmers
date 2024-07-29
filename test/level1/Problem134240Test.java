e7abdff48eb069e47b06b5e4e51c250b730c7360
너나들이 <tychejin@naver.com>
2024-07-29 17:46:13 +0900

    Chore: 코딩테스트 연습
    문제_134240 : 푸드 파이트 대회

---
+++ test/level1/Problem134240Test.java
@@ -0,0 +1,36 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem134240Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] food = {1, 3, 4, 6};
+    String expected = "1223330333221";
+
+    // When
+    String result = new Problem134240().solution(food);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] food = {1, 7, 1, 2};
+    String expected = "111303111";
+
+    // When
+    String result = new Problem134240().solution(food);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
