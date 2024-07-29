08561179a94092fb527f7729e35b47195be90f36
너나들이 <tychejin@naver.com>
2024-07-29 10:05:39 +0900

    Chore: 코딩테스트 연습
    문제_12901 : 2016년

---
+++ test/level1/Problem12901Test.java
@@ -0,0 +1,23 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Problem12901Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int a = 5;
+    int b = 24;
+    String expected = "TUE";
+
+    // When
+    String result = new Problem12901().solution(a, b);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
