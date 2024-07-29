b359ac1539b88a5d0383a345e389e19b159d3646
너나들이 <tychejin@naver.com>
2024-07-29 17:11:08 +0900

    Chore: 코딩테스트 연습
    문제_12903 : 가운데 글자 가져오기

---
+++ test/level1/Problem12903Test.java
@@ -0,0 +1,36 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem12903Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String s = "abcde";
+    String expected = "c";
+
+    // When
+    String result = new Problem12903().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String s = "qwer";
+    String expected = "we";
+
+    // When
+    String result = new Problem12903().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
