4b0afb375e12b84d34864dadd92dbcd5136ffa75
너나들이 <tychejin@naver.com>
2024-07-31 12:51:13 +0900

    Chore: 코딩테스트 연습
    문제_12906 : 같은 숫자는 싫어

---
+++ src/level1/Problem12906.java
@@ -0,0 +1,27 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 같은 숫자는 싫어
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ */
+public class Problem12906 {
+
+  public int[] solution(int[] arr) {
+
+    List<Integer> numList = new ArrayList<>();
+    numList.add(arr[0]);
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i - 1] != arr[i]) {
+        numList.add(arr[i]);
+      }
+    }
+
+    return numList.stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+  }
+}
