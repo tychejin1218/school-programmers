eabf7ab8d0c586c8c40089e53a4ee49f2b913f60
너나들이 <tychejin@naver.com>
2024-08-01 09:45:40 +0900

    Chore: 코딩테스트 연습
    문제_12916 : 문자열 내 p와 y의 개수

---
+++ src/level1/Problem12916.java
@@ -0,0 +1,21 @@
+package level1;
+
+/**
+ * 문자열 내 p와 y의 개수
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12916
+ */
+public class Problem12916 {
+
+  public boolean solution(String s) {
+
+    long pCount = s.toLowerCase().chars()
+        .filter(i -> i == 'p')
+        .count();
+    long yCount = s.toLowerCase().chars()
+        .filter(i -> i == 'y')
+        .count();
+
+    return pCount == yCount ? true : false;
+  }
+}
