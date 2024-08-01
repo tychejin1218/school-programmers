fac8ce9b45d2fd848cfed46b53fe814a5d184f42
너나들이 <tychejin@naver.com>
2024-08-01 09:56:47 +0900

    Chore: 코딩테스트 연습
    문제_12917 : 문자열 내림차순으로 배치하기

---
+++ src/level1/Problem12917.java
@@ -0,0 +1,19 @@
+package level1;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/**
+ * 문자열 내림차순으로 배치하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12917
+ */
+public class Problem12917 {
+
+  public String solution(String s) {
+    return s.chars()
+        .mapToObj(i -> String.valueOf((char) i))
+        .sorted(Comparator.reverseOrder())
+        .collect(Collectors.joining());
+  }
+}
---
+++ test/level1/Problem12917Test.java
@@ -0,0 +1,22 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem12917Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String s = "Zbcdefg";
+    String expected = "gfedcbZ";
+
+    // When
+    String result = new Problem12917().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
