52ad2b6b8aac9367229940fc0305adf19d6ee03a
너나들이 <tychejin@naver.com>
2024-08-05 10:11:51 +0900

    Chore: 코딩테스트 연습
    문제_12925 : 문자열을 정수로 바꾸기

---
+++ test/level1/Problem12925Test.java
@@ -0,0 +1,36 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem12925Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String s = "1234";
+    int expected = 1234;
+
+    // When
+    int result = new Problem12925().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String s = "-1234";
+    int expected = -1234;
+
+    // When
+    int result = new Problem12925().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
