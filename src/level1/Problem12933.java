a5fd2078d7790e29f7f8df5b6754d9c9e32693f9
너나들이 <tychejin@naver.com>
2024-08-07 13:23:00 +0900

    Chore: 코딩테스트 연습
    문제_12933 : 정수 내림차순으로 배치하기

---
+++ src/level1/Problem12933.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+/**
+ * 자연수 뒤집어 배열로 만들기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12933
+ */
+public class Problem12933 {
+
+  public long solution(long n) {
+    return Long.valueOf(
+        Arrays.stream(String.valueOf(n).split(""))
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.joining()));
+  }
+}
---
+++ test/level1/Problem12933Test.java
@@ -0,0 +1,22 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem12933Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    long n = 118372L;
+    long expected = 873211L;
+
+    // When
+    long result = new Problem12933().solution(n);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
