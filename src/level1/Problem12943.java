83c2a96674724bd5978f9213a613e1b488dec6c9
너나들이 <tychejin@naver.com>
2024-08-12 09:52:59 +0900

    Chore: 코딩테스트 연습
    문제_12943 : 콜라츠 추측

--- src/level1/Problem12943.java
+++ src/level1/Problem12943.java
@@ -1,7 +1,5 @@
 package level1;
 
-import java.util.stream.IntStream;
-
 /**
  * 콜라츠 추측
  * <p/>
@@ -9,7 +7,7 @@ import java.util.stream.IntStream;
  */
 public class Problem12943 {
 
-  public int solution01(int num) {
+  public int solution(int num) {
     long n = num;
     for (int i = 0; i < 500; i++) {
       if (n == 1) {
