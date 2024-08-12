b66c3b8f574c78de9e2979e13c45c00621d5ed52
너나들이 <tychejin@naver.com>
2024-08-12 09:52:40 +0900

    Chore: 코딩테스트 연습
    문제_12948 : 핸드폰 번호 가리기

---
+++ src/level1/Problem12948.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 핸드폰 번호 가리기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12948
+ */
+public class Problem12948 {
+
+  public String solution(String phone_number) {
+    char[] phoneNumbers = phone_number.toCharArray();
+    return IntStream.range(0, phoneNumbers.length)
+        .mapToObj(i -> i >= phoneNumbers.length - 4 ? phoneNumbers[i] : "*")
+        .map(String::valueOf)
+        .collect(Collectors.joining());
+  }
+}
