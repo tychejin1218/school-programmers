895e4b01ccbad6a92f1fa5bb80263bf624cb524b
너나들이 <tychejin@naver.com>
2024-08-31 22:17:04 +0900

    Chore: 코딩테스트 연습
    문제_76501 : 음양 더하기

---
+++ src/level1/Problem76501.java
@@ -0,0 +1,17 @@
+package level1;
+
+import java.util.stream.IntStream;
+
+/**
+ * 음양 더하기
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/76501
+ */
+public class Problem76501 {
+
+  public int solution(int[] absolutes, boolean[] signs) {
+    return IntStream.range(0, absolutes.length)
+        .map(i -> signs[i] ? absolutes[i] : -absolutes[i])
+        .sum();
+  }
+}
---
+++ test/level1/Problem76501Test.java
@@ -0,0 +1,38 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Problem76501Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    int[] absolutes = {4, 7, 12};
+    boolean[] signs = {true, false, true};
+    int expected = 9;
+
+    // When
+    int result = new Problem76501().solution(absolutes, signs);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int[] absolutes = {1,2,3};
+    boolean[] signs = {false,false,true};
+    int expected = 0;
+
+    // When
+    int result = new Problem76501().solution(absolutes, signs);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
