4e02f9bf0a4f973b46082a5eed37970135405d8a
너나들이 <tychejin@naver.com>
2024-09-13 09:54:10 +0900

    Chore: 코딩테스트 연습
    문제_135808 : 과일 장수

---
+++ src/level1/Problem135808.java
@@ -0,0 +1,25 @@
+package level1;
+
+import java.util.Arrays;
+
+/**
+ * 과일 장수
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/135808
+ */
+public class Problem135808 {
+
+  public int solution(int k, int m, int[] score) {
+    int answer = 0;
+
+    Arrays.sort(score);
+    for (int i = score.length - 1; i >= m - 1; i -= m) {
+      int minScore = score[i - (m - 1)];
+      if (minScore <= k) {
+        answer += minScore * m;
+      }
+    }
+
+    return answer;
+  }
+}
