7205b9e1bdb2cf15ec7d85f692b0162a92dac6a3
너나들이 <tychejin@naver.com>
2024-09-25 10:37:44 +0900

    Chore: 코딩테스트 연습
    문제_140108 : 문자열 나누기

---
+++ test/level1/Problem140108Test.java
@@ -0,0 +1,50 @@
+package level1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Problem140108Test {
+
+  @Test
+  void solution01() {
+
+    // Given
+    String s = "banana";
+    int expected = 3;
+
+    // When
+    int result = new Problem140108().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    String s = "abracadabra";
+    int expected = 6;
+
+    // When
+    int result = new Problem140108().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution03() {
+
+    // Given
+    String s = "aaabbaccccabba";
+    int expected = 3;
+
+    // When
+    int result = new Problem140108().solution(s);
+
+    // Then
+    assertEquals(expected, result);
+  }
+}
