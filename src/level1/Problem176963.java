073c5adc484cf5f1b1f4b99cc713ff7fe9204d16
너나들이 <tychejin@naver.com>
2024-10-21 13:42:07 +0900

    Chore: 코딩테스트 연습
    문제_176963 : 추억 점수

---
+++ src/level1/Problem176963.java
@@ -0,0 +1,30 @@
+package level1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 추억 점수
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/176963
+ */
+public class Problem176963 {
+
+  public int[] solution(String[] name, int[] yearning, String[][] photo) {
+
+    int[] answer = new int[photo.length];
+
+    Map<String, Integer> nameMap = new HashMap<>();
+    for (int i = 0; i < name.length; i++) {
+      nameMap.put(name[i], yearning[i]);
+    }
+
+    for (int i = 0; i < photo.length; i++) {
+      for (int j = 0; j < photo[i].length; j++) {
+        answer[i] += nameMap.getOrDefault(photo[i][j], 0);
+      }
+    }
+
+    return answer;
+  }
+}
