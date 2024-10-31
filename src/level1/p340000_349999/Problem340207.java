3e5c836596f8e82f0416e441e8e8bb4c3ad892c1
너나들이 <tychejin@naver.com>
2024-10-31 14:13:32 +0900

    Chore: 코딩테스트 연습
    문제_340207 : [PCCE 기출문제] 1번 / 문자 출력

---
+++ src/level1/p340000_349999/Problem340207.java
@@ -0,0 +1,16 @@
+package level1.p340000_349999;
+
+/**
+ * [PCCE 기출문제] 1번 / 문자 출력
+ * <p/>
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340207
+ */
+public class Problem340207 {
+
+  public static void main(String[] args) {
+    String message = "Let's go!";
+
+    System.out.println("3\n2\n1");
+    System.out.println(message);
+  }
+}
