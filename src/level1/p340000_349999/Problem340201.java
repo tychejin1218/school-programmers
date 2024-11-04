5af5f2cd593ed5d8451d98b769701528d9cdd564
너나들이 <tychejin@naver.com>
2024-11-04 10:48:20 +0900

    Chore: 코딩테스트 연습
    문제_340201 : [PCCE 기출문제] 7번 / 버스

--- src/level1/p340000_349999/Problem340201.java
+++ src/level1/p340000_349999/Problem340201.java
@@ -1,21 +1,56 @@
 package level1.p340000_349999;
 
 /**
- * [PCCE 기출문제] 6번 / 물 부족
+ * [PCCE 기출문제] 7번 / 버스
  * <p/>
- * https://school.programmers.co.kr/learn/courses/30/lessons/340202
+ * https://school.programmers.co.kr/learn/courses/30/lessons/340201
  */
-public class Problem340202 {
+public class Problem340201 {
 
-  public int solution(int storage, int usage, int[] change) {
-    int total_usage = 0;
-    for(int i=0; i<change.length; i++){
-      usage += usage * change[i] / 100;
-      total_usage += usage;
-      if(total_usage > storage){
-        return i;
+  public int solution(int seat, String[][] passengers) {
+    int num_passenger = 0;
+    for (int i = 0; i < passengers.length; i++) {
+      num_passenger += func4(passengers[i]);
+      num_passenger -= func3(passengers[i]);
+    }
+    System.out.println("num_passenger: " + num_passenger);
+    int answer = func1(seat - num_passenger);
+    return answer;
+  }
+
+  public int func1(int num) {
+    if (0 > num) {
+      return 0;
+    } else {
+      return num;
+    }
+  }
+
+  public int func2(int num) {
+    if (num > 0) {
+      return 0;
+    } else {
+      return num;
+    }
+  }
+
+  public int func3(String[] station) {
+    int num = 0;
+    for (int i = 0; i < station.length; i++) {
+      if (station[i].equals("Off")) {
+        num += 1;
       }
     }
-    return -1;
+    return num;
+  }
+
+  public int func4(String[] station) {
+    int num = 0;
+    for (int i = 0; i < station.length; i++) {
+      if (station[i].equals("On")) {
+        num += 1;
+      }
+    }
+    return num;
   }
 }
--- test/level1/p340000_349999/Problem340201Test.java
+++ test/level1/p340000_349999/Problem340201Test.java
@@ -1,4 +1,41 @@
-import static org.junit.jupiter.api.Assertions.*;
+package level1.p340000_349999;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 class Problem340201Test {
-  
+
+  @Test
+  void solution01() {
+
+    // Given
+    int seat = 5;
+    String[][] passengers = {{"On", "On", "On"}, {"Off", "On", "-"}, {"Off", "-", "-"}};
+    int expected = 3;
+
+    // When
+    int result = new Problem340201().solution(seat, passengers);
+
+    // Then
+    assertEquals(expected, result);
+  }
+
+  @Test
+  void solution02() {
+
+    // Given
+    int seat = 10;
+    String[][] passengers = {{"On", "On", "On", "On", "On", "On", "On", "On", "-", "-"},
+        {"On", "On", "Off", "Off", "Off", "On", "On", "-", "-", "-"},
+        {"On", "On", "On", "Off", "On", "On", "On", "Off", "Off", "Off"},
+        {"On", "On", "Off", "-", "-", "-", "-", "-", "-", "-"}};
+    int expected = 0;
+
+    // When
+    int result = new Problem340201().solution(seat, passengers);
+
+    // Then
+    assertEquals(expected, result);
+  }
 }
