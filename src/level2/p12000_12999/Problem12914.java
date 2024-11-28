7056cd809de88a8494f85a9f8cef20d7d22879cd
너나들이 <tychejin@naver.com>
2024-11-28 11:29:49 +0900

    Chore: 코딩테스트 연습
    문제_12914 : 멀리 뛰기

--- src/level2/p12000_12999/Problem12914.java
+++ src/level2/p12000_12999/Problem12914.java
@@ -7,7 +7,7 @@ package level2.p12000_12999;
  */
 public class Problem12914 {
 
-  public long solution(int n) {
+  public long solution01(int n) {
 
     long[] stepWayArr = new long[n + 1];
 
@@ -21,5 +21,27 @@ public class Problem12914 {
     }
 
     return stepWayArr[n];
+  }
+
+  public long solution(int n) {
+
+    if (n == 1) {
+      return 1;
+    }
+    if (n == 2) {
+      return 2;
+    }
+
+    long prev1 = 1;
+    long prev2 = 2;
+    long answer = 0;
+
+    for (int i = 3; i <= n; i++) {
+      answer = (prev1 + prev2) % 1234567;
+      prev1 = prev2;
+      prev2 = answer;
+    }
+
+    return answer;
   }
 }
